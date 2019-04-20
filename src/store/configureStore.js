bc8efe78c2484c23f68e0ba8987d52ff99cb45c7
hd.minh.pham <minh.pham@hdwebsoft.co>
2019-04-21 02:48:56 +0700

    fix store

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -2,10 +2,12 @@ import { createStore, applyMiddleware } from 'redux';
 import rootReducer from '../reducers';
 import createSagaMiddleware from 'redux-saga';
 
-export default configureStore = () => {
+const configureStore = () => {
   const sagaMiddleware = createSagaMiddleware()
     return {
       ...createStore(rootReducer, applyMiddleware(sagaMiddleware)),
       runSaga: sagaMiddleware.run,
     }
 }
+
+export default configureStore;
