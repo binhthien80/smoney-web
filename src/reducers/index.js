4f05cdd092cc31ef449392aae258c3066770503c
hd.minh.pham <minh.pham@hdwebsoft.co>
2019-04-21 02:41:58 +0700

    config redux-saga

---
+++ src/reducers/index.js
@@ -0,0 +1,10 @@
+import { combineReducers } from 'redux';
+
+
+//Reducer
+
+const rootReducer = combineReducers({
+    //some reducer
+})
+
+export default rootReducer;
---
+++ src/sagas/index.js
@@ -0,0 +1,11 @@
+// import { take, put, call, fork, select } from 'redux-saga/effects'
+// import fetch from 'isomorphic-fetch'
+// import * as actions from '../actions'
+
+//TODOS...
+
+
+
+export default function* root() {
+   
+}
