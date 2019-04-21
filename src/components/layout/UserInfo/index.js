c44097617b73b9486d61132ff214db1898ef4cc1
datmai <dat.mai@hdwebsoft.co>
2019-04-21 09:20:44 +0700

    fixed url warning

--- src/components/layout/UserInfo/index.js
+++ src/components/layout/UserInfo/index.js
@@ -10,7 +10,7 @@ export default class UserInfo extends React.Component {
             <span>Manage Smoney</span>
         </STYLE.Logo>
         <STYLE.UserInfo>
-            <img src="http://www.urbanui.com/equinox/template/images/faces/face28.jpg" />
+            <img src="http://www.urbanui.com/equinox/template/images/faces/face28.jpg" alt="avt"/>
             <p>Edwin Ballard</p>
         </STYLE.UserInfo>
         </div>
